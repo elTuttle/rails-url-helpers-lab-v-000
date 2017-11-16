dc18867ef83cd16a5811d1a00e7fc7fad6461175
John Tuttle <thejwt@gmail.com>
2017-11-16 23:54:54 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,12 +2,14 @@ class Student < ActiveRecord::Base
 
   attr_writer :active
 
+  after_initialize :set_attr
+
   def to_s
     self.first_name + " " + self.last_name
   end
 
-  def active
-    @active || false
+  def set_attr
+    @active = false
   end
 
 end
