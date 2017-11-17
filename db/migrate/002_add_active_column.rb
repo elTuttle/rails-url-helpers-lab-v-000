40c0b214f06847713d2bf010cc4570d08a827c3e
John Tuttle <thejwt@gmail.com>
2017-11-17 00:26:02 +0000

    automatically backed up by learn

--- db/migrate/002_add_active_column.rb
+++ db/migrate/002_add_active_column.rb
@@ -2,7 +2,7 @@ class AddActiveColumn < ActiveRecord::Migration
 
   def change
     change_table :students do |t|
-      t.boolean :active, :boolean, :null => false, :default => 0
+      t.boolean :active, :null => false, :default => 0
     end
   end
 
